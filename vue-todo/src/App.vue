1399d43c9d1be18efd82a021a13fdf189fe615f5
whddnjs117 <kydk1deram@gmail.com>
2024-02-12 19:58:50 +0900

    vue todo List

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput @addTodoItem="addOneItem"></TodoInput>
+    <TodoList :propsdata="todoItems" @removeItem="removeOneItem" @toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data : function() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addOneItem : function(todoItem) {
+      var obj = {completed : false, item: todoItem}
+      localStorage.setItem(todoItem,JSON.stringify(obj))
+      this.todoItems.push(obj)
+    },
+    removeOneItem : function(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index,1);
+    },
+    toggleOneItem : function(todoItem, index) {
+      todoItem.completed = !todoItem.completed
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+    }
+  },
+  created : function() {
+    if(localStorage.length > 0) {
+      for (var i=0; i< localStorage.length; i++) {
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+      }
+    }
+  },
+  components : {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color:#F6F6F6;
+}
+input {
+  border-style:groove;
+  width:200px;
+}
+button {
+  border-style:groove;
+
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem,index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+        <i class="fa-solid fa-check checkBtn" :class="{ checkBtnCompleted : todoItem.completed }"  @click="toggleComplete(todoItem, index)"></i>
+        <span :class="{textCompleted : todoItem.completed}">{{ todoItem.item }}</span>
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script scoped>
+export default {
+  props: ['propsdata'],
+  methods: {
+    removeTodo : function(todoItem, index) {
+      this.$emit('removeItem',todoItem,index)
+    },
+    toggleComplete : function(todoItem, index) {
+      this.$emit('toggleItem',todoItem,index)
+    }
+  },
+
+}
+</script>
+
+<style>
+ul {
+  list-style-type : none;
+  padding-left:0px;
+  margin-top:0;
+  text-align:left;
+}
+li {
+  display: flex;
+  min-height: 50x;
+  height:50px;
+  line-height:50px;
+  margin:0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius:5px;
+}
+.removeBtn {
+  margin-left:auto;
+  color:#de4343;
+}
+.checkBtn {
+  line-height:45px;
+  color:#62acde;
+  margin-right:5px;
+}
+.checkBtnCompleted {
+  color:#b3adad;
+}
+.textCompleted {
+  text-decoration:line-through;
+  color:#b3adad
+}
+
+</style>
